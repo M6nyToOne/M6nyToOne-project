70b2d7674d588d53a927c1cff89758ec03e9a20b
jasmin <cjm98@smartel.co.kr>
2026-02-19 20:43:58 +0900

    Init : 프로젝트 생성

---
+++ src/main/java/sparta/m6nytooneproject/M6nyToOneProjectApplication.java
@@ -0,0 +1,13 @@
+package sparta.m6nytooneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M6nyToOneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M6nyToOneProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sparta/m6nytooneproject/M6nyToOneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.m6nytooneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M6nyToOneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
