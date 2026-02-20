e41b0aa24dd777536475a9e0288398cccb10e8d2
pposong0625 <posong0625@gmail.com>
2026-02-20 20:52:26 +0900

    feat: 상품 삭제 완성

--- src/main/java/sparta/m6nytooneproject/product/dto/UpdateProductStockRequestDto.java
+++ src/main/java/sparta/m6nytooneproject/product/dto/UpdateProductStockRequestDto.java
@@ -1,9 +1,11 @@
 package sparta.m6nytooneproject.product.dto;
 
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 
 @Getter
 public class UpdateProductStockRequestDto {
 
+    @Min(value = 0, message = "재고는 0개 이상이어야 합니다.")
     private int stock;
 }
