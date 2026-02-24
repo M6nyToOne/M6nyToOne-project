7d6c095414af1346e5dd64472e59e435a68a2dba
jasmin <cjm98@smartel.co.kr>
2026-02-24 16:31:30 +0900

    Fix : 주문 최소 수량 변경

--- src/main/java/sparta/m6nytooneproject/order/dto/OrderRequestByCustomerDto.java
+++ src/main/java/sparta/m6nytooneproject/order/dto/OrderRequestByCustomerDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 public class OrderRequestByCustomerDto {
-    @Min(value = 0 , message = "수량은 0개 이상 이어야 합니다.")
+    @Min(value = 1 , message = "수량은 1개 이상 이어야 합니다.")
     private Integer quantity;
     @Min(1)
     private Long productId;
